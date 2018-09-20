2ae557a8140096e8c6ba38aaadc5deb3e23b84ab
fernando sierra <fernandosierra9@gmail.com>
2018-09-19 23:21:11 -0300

    tercer paso

---
+++ SAFA/Debug/src/SAFA.d
@@ -0,0 +1,7 @@
+src/SAFA.o: ../src/SAFA.c \
+ /home/utnso/git/TPOP/libreria/commons/temporal.h \
+ /home/utnso/git/TPOP/libreria/commons/fer.h
+
+/home/utnso/git/TPOP/libreria/commons/temporal.h:
+
+/home/utnso/git/TPOP/libreria/commons/fer.h:
---
+++ libreria/Debug/commons/fer.d
@@ -0,0 +1,3 @@
+commons/fer.o: ../commons/fer.c ../commons/fer.h
+
+../commons/fer.h:
